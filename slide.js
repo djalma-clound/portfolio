2baa75b3b78ba0cc51d6c179e31781bd33614aa1
djalma-clound <79033840+djalma-clound@users.noreply.github.com>
2022-02-24 18:53:39 -0800

    Update slide.js

--- slide.js
+++ slide.js
@@ -1,1 +1,27 @@
 
+var imgObj = null;
+var animate ;
+
+function init() {
+   imgObj = document.getElementById('theCover');
+   imgObj.style.position= 'relative'; 
+   imgObj.style.left = '0px'; 
+}
+function moveRight() {
+   imgObj.style.left = parseInt(imgObj.style.left) + 10 + 'px';
+   animate = setTimeout(moveRight,10);    // call moveRight in 20msec
+}
+
+function stop() {
+   clearTimeout(animate);
+   imgObj.style.left = '0px'; 
+}
+function entry() {
+   clearTimeout(animate);
+   imgObj.style.left = '0px'; 
+ animate = setTimeout(moveRight,10);    
+}
+
+
+window.onload = init;
+
