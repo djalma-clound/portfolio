386479e5fc7513bc73ff95a944a41f53dc18a2c8
djalma <79033840+djalma-clound@users.noreply.github.com>
2022-09-12 16:22:53 -0400

    Update date.js

--- js/date.js
+++ js/date.js
@@ -2,4 +2,4 @@ var today = new Date();
 var year = today.getFullYear();
 
 var el = document.getElementById('footer');
-el.innerHTML = '<p> &copy;' + year +  '  Dj-techs all right reserved!' + '</p>' + ' <h4>Contact me for more information</h4> ' + ' <a href="contact.html">Contact-me</a>' + '<p class="email"> Djalmas@dj-techs.com</p>';
+el.innerHTML = '<p> &copy;' + year +  '  Dj-techs all right reserved!' + '</p>' + ' <h4>Contact me for more information</h4> ' + ' <a href="contact1.html">Contact-me</a>' + '<p class="email"> Djalmas@dj-techs.com</p>';
