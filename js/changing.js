8515b793254c0f64f1682a8987392a4dfdfa77ca
djalma <79033840+djalma-clound@users.noreply.github.com>
2022-09-12 15:28:05 -0400

    Delete js directory

--- js/changing.js
+++
@@ -1,9 +0,0 @@
-const sect = document.getElementById('div-sect');
-
-window.addEventListener('scroll', () => {
-    if (window.scrollY >= 1) {
-        sect.classList.add('active-div');
-    } else {
-        sect.classList.remove('active-div');
-    }
-})
--- js/nav.js
+++
@@ -1,9 +0,0 @@
-const nav = document.querySelector('nav');
-
-window.addEventListener('scroll', () => {
-    if (window.scrollY >= 50) {
-        nav.classList.add('active-nav');
-    } else {
-        nav.classList.remove('active-nav');
-    }
-})
