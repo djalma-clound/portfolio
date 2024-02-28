3bf119b4b44954a9f8c06f8f83a3c40cc5935a05
djalma-clound <djalmad841@gmail.com>
2024-02-28 12:47:54 -0500

    fixing video

--- Menu.js
+++ Menu.js
@@ -1,7 +1,7 @@
 const bars = document.getElementById("bars");
 const menu = document.getElementById("menu");
 const navigation = document.getElementById("nav");
-const button = document.getElementById("button");
+const button = document.querySelector("#button");
 bars.addEventListener("click", () => {
   navigation.classList.toggle("activ");
   menu.classList.toggle("active");
@@ -13,7 +13,7 @@ bars.addEventListener("click", () => {
     });
   });
 });
-const img = document.getElementById("img1");
+const img = document.getElementById("#button");
 const img2 = document.getElementById("img2");
 const img3 = document.getElementById("img3");
 img.addEventListener("click", ()=>{
@@ -26,6 +26,7 @@ img3.addEventListener("click", () => {
   window.open("https://djalma-clound.github.io/portfolio/index.html", "_blank");
 });
 
-button.addEventListener("click", ()=>{
-  window.location.href = "#contact";
+button.addEventListener("click", () => {
+ window.open("https://www.scentedcleaning.com", "_blank");
+  window.prompt("everything ok");
 })
