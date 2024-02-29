14fd2ef70e60da0fafcbc843eba76ebae91454f4
djalma-clound <djalmad841@gmail.com>
2024-02-29 13:55:08 -0500

    adding text and fixing some parts

--- Menu.js
+++ Menu.js
@@ -1,6 +1,8 @@
 const bars = document.getElementById("bars");
 const menu = document.getElementById("menu");
 const navigation = document.getElementById("nav");
+const header = document.querySelector("header");
+const logo = document.getElementById("logo");
 const button = document.querySelector("#button");
 bars.addEventListener("click", () => {
   navigation.classList.toggle("activ");
@@ -13,6 +15,29 @@ bars.addEventListener("click", () => {
     });
   });
 });
+window.onscroll = function () {
+  let currentScrollPos = window.pageYOffset;
+  if (prevScrollPos > currentScrollPos) {
+    // Scrolling up
+    logo.style.color = "white";
+    header.style.backgroundColor = "lightgreen";
+    bars.style.backgroundColor = "white";
+
+    menu.forEach(function (item) {
+      item.style.color = "white";
+    });
+  } else {
+    // Scrolling down
+    logo.style.color = "red";
+    bars.style.backgroundColor = "red";
+
+    header.style.backgroundColor = "white";
+    menu.forEach(function (item) {
+      item.style.color = "red";
+    });
+  }
+  prevScrollPos = currentScrollPos;
+};
 const proj1 = document.getElementById("proj1");
 const proj2 = document.getElementById("proj2");
 
